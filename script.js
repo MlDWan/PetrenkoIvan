0aa1440b344b32172f4f6bb78c78885ef5ca7a0f
mornigar11001 <57406611+mornigar11001@users.noreply.github.com>
2020-07-16 13:57:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    //loop:true,
+    nav:true,
+    margin: 10,
+    responsive:{
+        870:{
+            items:1
+        },
+        1215:{
+
+            items:2
+        },
+        1460:{
+            items:3
+        }
+    }
+})
